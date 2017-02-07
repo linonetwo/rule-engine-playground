50b17adb50bf1b7be5916f7723e27d8cbf41bd61
linonetwo <linonetwo012@gmail.com>
2017-02-07 14:06:22 +0800

    feat: parse simple rule by elixir

--- rule_engine_playground/lib/rule_engine_playground.ex
+++ rule_engine_playground/lib/rule_engine_playground.ex
@@ -13,6 +13,47 @@ defmodule RuleEnginePlayground do
 
   """
   def hello do
-    :world
+    :worlds
+  end
+
+  @doc """
+  <~>
+  not in
+  find whether a value is not inside a list
+  """
+  defp value <~> list do
+    Enum.find_value(list, false, &(&1 != value))
+  end
+
+  defp format_compare(rule) do
+    re_less_than_frag = ~r/(\S+) less than (\S+)/i
+    re_large_than_frag = ~r/(\S+) large than (\S+)/i
+    re_equal_to_frag = ~r/(\S+) equal to (\S+)/i
+
+    rule
+    |> (fn (rule) -> Regex.replace(re_less_than_frag, rule, "^\\g{1} < \\g{2}") end).()
+    |> (fn (rule) -> Regex.replace(re_large_than_frag, rule, "^\\g{1} > \\g{2}") end).()
+    |> (fn (rule) -> Regex.replace(re_equal_to_frag, rule, "^\\g{1} = \\g{2}") end).()
+  end
+
+  @doc """
+  exampel:
+    format_is("aaa is bbb")
+    "^aaa = bbb"
+  """
+  def format_is(rule) do
+    re_is_literal_frag = ~r/(\S+) is "(\S+)"/i
+    re_is_atom_frag = ~r/(\S+) is ([^"\s]+)/i
+
+    rule
+    |> (fn (rule) -> Regex.replace(re_is_literal_frag, rule, "^\\g{1} = \"\\g{2}\"") end).()
+    |> (fn (rule) -> Regex.replace(re_is_atom_frag, rule, "^\\g{1} = :\\g{2}") end).()
+  end
+
+  def parse(rule) do
+    rule
+    |> format_is
+    |> format_compare
+    # |> Code.string_to_quoted
   end
 end
