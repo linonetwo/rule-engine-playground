bd8bc0d24037ed446ea86a28990d974a5fad3cb8
linonetwo <linonetwo012@gmail.com>
2017-02-07 16:49:54 +0800

    feat: policy enging try

--- rule_engine_playground/lib/rule_engine_playground.ex
+++ rule_engine_playground/lib/rule_engine_playground.ex
@@ -1,4 +1,5 @@
 defmodule RuleEnginePlayground do
+  require Logger
   @moduledoc """
   Documentation for RuleEnginePlayground.
   """
@@ -14,6 +15,7 @@ defmodule RuleEnginePlayground do
   """
   def hello do
     :worlds
+    Logger.warn("useless function call")
   end
 
   @doc """
@@ -27,12 +29,12 @@ defmodule RuleEnginePlayground do
 
   defp format_compare(rule) do
     re_less_than_frag = ~r/(\S+) less than (\S+)/i
-    re_large_than_frag = ~r/(\S+) large than (\S+)/i
+    re_larger_than_frag = ~r/(\S+) larger than (\S+)/i
     re_equal_to_frag = ~r/(\S+) equal to (\S+)/i
 
     rule
     |> (fn (rule) -> Regex.replace(re_less_than_frag, rule, "^\\g{1} < \\g{2}") end).()
-    |> (fn (rule) -> Regex.replace(re_large_than_frag, rule, "^\\g{1} > \\g{2}") end).()
+    |> (fn (rule) -> Regex.replace(re_larger_than_frag, rule, "^\\g{1} > \\g{2}") end).()
     |> (fn (rule) -> Regex.replace(re_equal_to_frag, rule, "^\\g{1} = \\g{2}") end).()
   end
 
@@ -51,9 +53,34 @@ defmodule RuleEnginePlayground do
   end
 
   def parse(rule) do
+    Logger.warn("Parsing rule:  #{rule}")
     rule
     |> format_is
     |> format_compare
-    # |> Code.string_to_quoted
+    |> Code.string_to_quoted
+  end
+
+  @doc """
+    Try by
+      funcs = RuleEnginePlayground.parse_db()
+      Enum.at(funcs, 0).()
+  """
+  def parse_db() do
+    rules = [
+      %{id: 0, rule: "type is \"dht11\" and metric is temperature and value less than 50"},
+      %{id: 1, rule: "type is \"dht12\" and metric is current and value larger than 50"},
+    ]
+
+
+    rules
+    |> Enum.to_list
+    |> Enum.map(fn %{:id => id, :rule => rule} ->
+      case parse(rule) do
+        {:ok, quoted_rule} ->
+          fn -> Code.eval_quoted(quoted_rule)
+          end
+        {:error, {_, error, token}} -> Logger.warn("Quote Failed: {#{id}, #{rule}}")
+      end
+    end)
   end
 end
